260dd21703b9eb31b3e22c1664106c5d938a9951
Nicholas Boshart <nichabosh.code@gmail.com>
2020-08-16 10:37:10 -0400

    Added Scale, Zoom, & Navigation
    • Updated index.html to include incremental navigation buttons.
    • Scale and zoom controls are added to map upon first loading in.
    • Created scripts/utils/props.js for easy access to map properties.
    • Introduced application "state" variable in app.js.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,15 @@
+import props from "./utils/props.js";
+
+// application state
+const state = {
+  style: props.styles.dark,
+  city: props.cities["Atlanta"]
+};
+
+// ==========================
+//      CREATING THE MAP
+// ==========================
+
 // associates api requests w/ mapbox account
 mapboxgl.accessToken = "pk.eyJ1IjoiYXRsYW50YS"
   + "1tYXByb29tIiwiYSI6ImNrYmU2NXNyNjBpb2MyeG"
@@ -6,7 +18,29 @@ mapboxgl.accessToken = "pk.eyJ1IjoiYXRsYW50YS"
 // creates and renders map in index.html
 const map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/dark-v8",
-  center: [-84.3924, 33.7488],
+  style: state.style,
+  center: state.city,
   zoom: 12
 });
+
+map.on("load", () => {
+  map.getCanvas().focus();
+
+  // geographic scale and zoom functionalities
+  const scale = new mapboxgl.ScaleControl({ unit: "imperial" });
+  const zoom = new mapboxgl.NavigationControl();
+  map.addControl(scale, "bottom-left");
+  map.addControl(zoom, "bottom-left");
+});
+
+// ===================================
+//      INTERACTION WITH ELEMENTS
+// ===================================
+
+// the "window" object is necessary because main.js is imported
+// as a module in index.html (reference: https://bit.ly/2YnNpLc).
+
+window.handleMovement = (x, y) => {
+  // incrementally moves map
+  map.panBy([x, y]);
+};
