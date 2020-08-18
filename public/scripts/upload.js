f0d00d527c777d934c59f96198dd423b7bbd1ace
Nicholas Boshart <nichabosh.code@gmail.com>
2020-08-18 19:23:50 -0400

    Finished /api/addmap & Map Database
    • Made post request in upload.js & created html response messages.
    • Added necessary logic to api/addmap.js (i.e. errors, db, etc).
    • Tweaked input text color & added response styling to upload.css.
    • Created queue of uploaded maps in /data/maps.json.
       -> Approved and active maps will be added to map display.

--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -1,6 +1,8 @@
-let mapname, styleurl, password;
+// form state & html elements
+let mapname = styleurl = password = null;
 const form = document.getElementsByTagName("form")[0];
 const button = document.getElementsByTagName("button")[0];
+const message = document.getElementsByTagName("p")[0];
 
 // mutator functions used by form to update state
 function handleUrl(e) { styleurl = e.target.value }
@@ -14,8 +16,27 @@ form.addEventListener("submit", async (e) => {
   button.innerHTML = "Processing";
   button.disabled = true;
 
-  // todo: add api call to /api/addmap
-  await new Promise(resolve => setTimeout(resolve, 3000));
+  // sets mandatory wait time between requests (1s)
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  // addmap post request
+  fetch("/api/addmap", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ mapname, styleurl, password })
+  })
+    .then(async response => {
+      // configures helper text beneath submit button
+      message.innerHTML = await response.text();
+      message.style.color = response.status === 200
+        ? "#80cba0" : "#e77e7e";
+
+      // resets form state on successful upload
+      if (response.status === 200) {
+        mapname = styleurl = password = null;
+        form.reset();
+      }
+    });
 
   // re-enable submissions
   button.disabled = false;
--- routes/api/addmap.js
+++ routes/api/addmap.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const parser = require("body-parser");
 const express = require("express");
 const router = express.Router();
@@ -6,8 +7,38 @@ const router = express.Router();
 require("dotenv").config();
 
 router.post("/addmap", parser.json(), (req, res) => {
-  // todo: add necessary logic
-  res.status(200).end();
+  // form data passed through request body
+  const { mapname, styleurl, password } = req.body;
+
+  // helper response functions
+  const accept = (msg) => res.status(200).send(msg);
+  const reject = (err) => res.status(400).send(err);
+
+  // load previously-uploaded data
+  const file = fs.readFileSync("data/maps.json", "utf-8");
+  const data = JSON.parse(file);
+
+  // checks for input-related errors
+  if (!mapname || !styleurl || !password) {
+    reject("Please fill out all required information.");
+  } else if (password !== process.env.password) {
+    reject("Password is incorrect. Try again.");
+  } else if (data[mapname]) {
+    reject(`"${mapname}" has already been uploaded.`);
+  } else {
+    // append new data
+    data[mapname] = {
+      url: styleurl,
+      date: Date.now(),
+      approved: false,
+      active: false
+    };
+
+    // apply formatting & overwrite existing json
+    let appended = JSON.stringify(data, null, 2);
+    fs.writeFile("data/maps.json", appended, () => {});
+    accept("Your map is pending approval.");
+  }
 });
 
 module.exports = router;
